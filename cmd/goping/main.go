0aa157c752b29f55673773ed6f6a5bacafc8918b
Albert Le Batteux <ale-batt@student.42.fr>
2017-03-15 22:23:07 +0100

    [+] Simple ping working

--- cmd/goping/main.go
+++ cmd/goping/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/ale-batt/goping"
+import (
+	"os"
+
+	"github.com/ale-batt/goping"
+)
 
 func main() {
-	goping.Ping("google.fr")
-	goping.Ping("216.58.208.195")
+	if os.Args[1] != "" {
+		goping.Ping(os.Args[1])
+	}
 }
--- goping.go
+++ goping.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"time"
 
 	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
 )
 
 type Pinger struct {
@@ -14,28 +17,65 @@ type Pinger struct {
 	socket   *icmp.PacketConn
 }
 
-func newPinger(addr string) *icmp.PacketConn {
-	ip, errIp := net.LookupIP(addr)
-	if errIp != nil {
-		log.Print("Bad ip")
-	}
+func newPinger(addr string) *Pinger {
+	var p Pinger
 
-	fmt.Println(ip[0].String())
-	sock, err := icmp.ListenPacket("ip4:icmp", addr)
+	ip, err := net.LookupIP(addr)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return sock
+	p.hostip = ip[0].String()
+	p.hostname = addr
+
+	p.socket, err = icmp.ListenPacket("ip4:icmp", "0.0.0.0")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &p
 }
 
-func ping() {
+func (p *Pinger) sendICMP() {
+	packet := icmp.Message{
+		Type:     ipv4.ICMPTypeEcho,
+		Code:     0,
+		Checksum: 0, // think to add
+	}
+	body := &icmp.Echo{
+		ID:   os.Getpid() & 0xffff,
+		Seq:  0,
+		Data: []byte("toc toc toc"),
+	}
+	for {
+		body.Seq += 1
+		packet.Body = body
+		pkt, err := packet.Marshal(nil)
+		if err != nil {
+			log.Fatal(err)
+		}
 
+		cc, err := (p.socket).WriteTo(pkt, &net.IPAddr{IP: net.ParseIP(p.hostip)})
+		if err != nil || cc == 0 {
+			log.Fatal(err)
+		}
+		//fmt.Println("send", cc, "bytes")
+		time.Sleep(time.Second * 1)
+	}
 }
 
-func listener() {
+func (p *Pinger) recvICMP() {
+	buff := make([]byte, 512)
 
+	for {
+		cc, peer, err := (p.socket).ReadFrom(buff)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(cc, "bytes from", peer)
+	}
 }
 
 func Ping(addr string) {
-	newPinger(addr)
+	p := newPinger(addr)
+	go p.sendICMP()
+	p.recvICMP()
 }
